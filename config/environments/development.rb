9ce0a405981cc79966d59b27307c40f12332a23f
Merili Martverk <merili.martverk@gmail.com>
2020-05-28 03:01:55 +0300

    devise authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+  
+  Rails.application.routes.default_url_options[:host] = 'domain.com'
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
